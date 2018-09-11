0c7a053b874eec161a923e3b6d2f951a4f665a11
UTKARSH-PC\utkar <utkarsh.kasana@gmail.com>
2018-09-11 12:52:48 -0400

    no message

---
+++ public/scripts/app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'restangular'
+  ])
+  .config(function ($routeProvider,$locationProvider,RestangularProvider) {
+
+      RestangularProvider.setBaseUrl('http://localhost:3000');
+
+      $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'Movies1Ctrl',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+      $locationProvider.hashPrefix('');
+
+     //$locationProvider.html5Mode(true);
+
+
+  })
+    .factory('MovieRestangular', function (Restangular) {
+        return Restangular.withConfig(function(RestangularConfigurer){
+            RestangularConfigurer.setRestangularFields({
+                id: '_id'
+            });
+        });
+    })
+    .factory('Movie', function (MovieRestangular) {
+        return MovieRestangular.service('movie');
+    });
