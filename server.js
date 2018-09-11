e3165323021bd0af8f6476c6075f5d9f9adb7a24
UTKARSH-PC\utkar <utkarsh.kasana@gmail.com>
2018-09-11 14:13:50 -0400

    no message

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-// Connect to the database before starting the application server. 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+// Connect to the database before starting the application server.
+var mongodb_uri = "mongodb://utkarshkasana:*Indian88@ds257838.mlab.com:57838/utkarshkasana1";
+mongodb.MongoClient.connect(mongodb_uri, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
